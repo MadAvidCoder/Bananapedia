b40190c2c94a784f93e3e2d1f523f15f6b65e6d0
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-03-04 21:18:11 +1100

    Build for CLI Edited the file to support a CLI for easier operation.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from tools.wav_decoder import wav_to_json
 from tools.downloader import download
+import argparse
 
-data = wav_to_json('encoded.wav')
-download(data, 'decoded_banana.png')
-print("Success! File is saved to: decoded_banana.png")
+parser = argparse.ArgumentParser()
+parser.add_argument("input", type=str, help="Path to the encoded *.wav file.")
+parser.add_argument("-o", "--output", default="decoded_banana.png", type=str, help="Path to output the decoded *.png file.")
+
+def main():
+    args = parser.parse_args()
+    data = wav_to_json(args.input)
+    download(data, args.output)
+    print(f"Success! File is saved to: {args.output}")
+    return 0
+
+if __name__ == "__main__":
+    raise SystemExit(main())
